4bce286195ae93d41131ccbb21924b388cdab27b
Daniel Diepold <daniel.diepold@exxcellent.de>
2013-10-18 15:17:05 +0200

    Revert optimization when checking for changed properties
    oldValue == newValue == null can actually indicate a change to a
    component, for example the list model of a select field.

--- src/server-java/app/nextapp/echo/app/ApplicationInstance.java
+++ src/server-java/app/nextapp/echo/app/ApplicationInstance.java
@@ -603,7 +603,7 @@ implements Serializable {
             }
             serverUpdateManager.processComponentVisibilityUpdate(parent);
         } else {
-            if ((oldValue != null && newValue != null && oldValue.equals(newValue)) || (oldValue == null && newValue == null)) {
+            if (oldValue != null && newValue != null && oldValue.equals(newValue)) {
                 return;
             }
             if (parent instanceof ModalSupport && ModalSupport.MODAL_CHANGED_PROPERTY.equals(propertyName)) {
